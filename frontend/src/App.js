7b80c5d646e981d1d63ff5059e2b196f588ff45d
nsachdeva2 <nandinisachdeva27@gmail.com>
2026-02-07 20:56:30 -0500

    Updated frontend and backend structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,4 @@
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
-
-function IntakeForm() {
-  const navigate = useNavigate();
-
-  return (
-    <div>
-      <h1>Intake Form</h1>
-      <button onClick={() => navigate("/homepage")}>
-        Submit Form
-      </button>
-    </div>
-  );
-}
-
-function ResultsPage() {
-  return <h1>Results Page</h1>;
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
